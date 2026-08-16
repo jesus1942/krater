a5f03c8b2796907f88aac0e1cf1a84cd4a9b2130
Jesus Olguin <47510848+jesus1942@users.noreply.github.com>
2026-08-16 20:42:07 +0000

    Aislar métricas del dashboard por nivel

--- app/Http/Controllers/V1/Dashboard/DashboardController.php
+++ app/Http/Controllers/V1/Dashboard/DashboardController.php
@@ -14,6 +14,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('tenant');
+    }
+
     /**
      * Handle the incoming request.
      *
